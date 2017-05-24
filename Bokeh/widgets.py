4e61ab4531a9fecab561e4854b4584aa20f1ead1
Sha Li <sliwhu@gmail.com>
2017-05-24 10:59:01 -0700

    interactive bokeh on price prediction

--- Bokeh/widgets.py
+++ Bokeh/widgets.py
@@ -7,32 +7,61 @@ open a web brower and go to localhost:5006, the page will show
 '''
 
 
-from bokeh.io import curdoc, output_file, show
+from bokeh.io import curdoc, output_file, show, reset_output
 from bokeh.layouts import widgetbox, layout
 from bokeh.models.widgets import Button, TextInput, RadioButtonGroup, Select, Slider, Paragraph
 
 
-# create some widgets
-text_input = TextInput(value='Lisa')
-select1 = Select(title="Bedroom number:", value="0", options=['2', '3', '4', '5'])
-select2 = Select(title="Bathroom number:", value="0", options=['2', '3', '4', '5'])
-select3 = Select(title="Sqft:", value="0", options=['1000', '1500', '2000', '2500'])
-select4 = Select(title="Built year:", value="0", options=['2', '3', '4', '5'])
-
+# Create Input Controls 
+bed = Select(title="Bedroom number:", value="0", options=['2', '3', '4', '5'])
+bath = Select(title="Bathroom number:", value="0", options=['2', '3', '4', '5'])
+sqft = Slider(title="Sqft:", value=1800, start=500, end=5500, step=10)
+builtyear = Slider(title="Built year:", value=2000, start=1960, end=2015, step=1)
+waterfront = Select(title="Waterfront:", value="either", options=['Yes', 'No', 'either'])
+renovation = Select(title="Renovation:", value="either", options=['Yes', 'No', 'either'])
+zipcode = TextInput(title="enter zipcode here")
 
 button_1 = Button(label="Submit")
 button_2 = Button(label="Reset")
-output = Paragraph()
+output = Paragraph(width=600, height=300) #or use pretext, for a <pre> tag in html
+
+def determine(parameter):
+	if parameter.value == 'Yes':
+		parameter.value = str(1)
+	elif parameter.value == 'No':
+		parameter.value = str(0)
+	else:
+		parameter.value = str(0.5)
 
 def submit():
-	coefficients = [100000, 10000, 1, 1]
-	# value = select1.value * coefficients[0]  
-	# + select2.value * coefficients[1] + \
- #    select3.value * coefficients[2] + select4.value*coefficients[3]
-	output.text = 'Your house is predicted as, ' + select3.value + '$'
+	#these are made up coefficients for now
+	coefficients = [100000, 10000, 1, 1, 5, 5]
+	determine(waterfront)
+	determine(renovation)
+	value = float(bed.value) * coefficients[0] + float(bath.value) * coefficients[1] + \
+ 		 float(sqft.value) * coefficients[2] + float(builtyear.value)*coefficients[3] + \
+ 		 float(waterfront.value)*coefficients[4] + float(renovation.value)*coefficients[5]
+	output.text = 'Your house is predicted as, ' + str(value) + ' $'
+	
+def reset():
+	reset_output()
+	output.text = None
 
 button_1.on_click(submit)
 
-lay_out = layout([[select1, select2, select3, select4, button_1, button_2], [output]])
+button_2.on_click(reset)
 
+#lay_out = layout([[select1, select2, select3, select4], [button_1, button_2], [output]])
+lay_out = layout(
+			children=[
+			[bed, bath],
+			[sqft, builtyear],
+			[waterfront, renovation],
+			[button_1],
+			[button_2],
+			[output]
+			],
+			sizing_mode='fixed',
+			)
 curdoc().add_root(lay_out)
+curdoc().title = "Predict the price of your first home"
