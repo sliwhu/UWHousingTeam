26a03a8ebfd2528456ef384a7ddb08757abad2fe
Gary Gregg <garygr@uw.edu>
2017-06-03 12:08:45 -0700

    This source file is no longer in use, but it has been modified to clear pylint warnings.

--- model/house_price_model.py
+++ model/house_price_model.py
@@ -3,8 +3,9 @@ Contains the house price model.
 
 DON'T USE THIS MODEL!  Use the HousePriceModel in house_price_model_2.py.
 """
-import numpy as np
 import os
+
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import RidgeCV
