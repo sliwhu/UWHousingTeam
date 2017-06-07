28af0f4edb7aa7edc3093839e69999501af80102
Deepa Agrawal <deepa15@uw.edu>
2017-06-06 22:34:14 -0700

    adding setup.py

---
+++ UWHousingTeam/Scripts/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PACKAGES',
+    version='0.1',
+    packages=['csv','os',],
+    url='https://github.com/sliwhu/UWHousingTeam',
+    license='MIT',
+    author='dipsm',
+    author_email='deepa15@uw.edu',
+    description='First Stop for First Time Home Buyers'
+)
