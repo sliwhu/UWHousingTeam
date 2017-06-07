cbf1237bbcfd403695d5d69337c5e75d0b1eac33
Deepa Agrawal <deepa15@uw.edu>
2017-06-07 01:14:56 -0700

    updated setup file

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ PACKAGES = find_packages()
 opts = dict(
     name='UWHousingTeam',
     version='0.1',
-    packages=PACKAGES,
-    package_data={'UWHousingTeam': ['Scripts/*', 'tests/*']},
     url='https://github.com/sliwhu/UWHousingTeam',
     license='MIT',
-    author='dipsm',
+    author='firststop',
     author_email='deepa15@uw.edu',
     description='First Stop for First Time Home Buyers'
+    packages=PACKAGES,
+    package_data={'UWHousingTeam': ['Scripts/*', 'tests/*']},
+ 
 )
 
 if __name__ == '__main__':
