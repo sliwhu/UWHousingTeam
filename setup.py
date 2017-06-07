de922d185082cb56ccb8d54b6f9e4df3c71d0cb4
Deepa Agrawal <deepa15@uw.edu>
2017-06-07 00:16:57 -0700

    adding setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+PACKAGES = find_packages()
+
+
+opts = dict(
+    name='UWHousingTeam',
+    version='0.1',
+    packages=PACKAGES,
+    package_data={'UWHousingTeam': ['Scripts/*', 'tests/*']},
+    url='https://github.com/sliwhu/UWHousingTeam',
+    license='MIT',
+    author='dipsm',
+    author_email='deepa15@uw.edu',
+    description='First Stop for First Time Home Buyers'
+)
+
+if __name__ == '__main__':
+    setup(**opts)
